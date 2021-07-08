15b982b337aab33d8a0eed360c55b4347a1637fe
Allaye <kolade199@gmail.com>
2021-07-08 18:11:09 +0000

    add

--- todos/pagination.py
+++ todos/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'count'
-    page_size = 10
+    page_size = 4
     max_page_size = 100
-    page_query_param = 'p'
-    
+    page_query_param = 'page'
+
